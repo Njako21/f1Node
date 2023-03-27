8c7290f79a181f76797cdbe24a14ae46c3a1f0c1
Njako21 <njako21@student.sdu.dk>
2023-03-27 13:19:24 +0200

    refactoring
    re

--- routes/f1.js
+++ routes/f1.js
@@ -1,19 +1,13 @@
+const https = require('https');
 const express = require('express');
 const router = express.Router();
 
 // Define routes for /api/f1
 // GET /api/users
 router.get('/', (req, res) => {
-    fetch('https://www.formula1.com/en/results.html/2023/races/1142/saudi-arabia/race-result.html')
-      .then(response => response.text())
-      .then(data => {
-        res.send(data);
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('An error occurred while fetching the data.');
-      });
-  });
+  jsonFormat = JSON.stringify(test);
+  res.send(jsonFormat);
+});
   
   // get year
   router.get('/:year', (req, res) => {
