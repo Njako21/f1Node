aa3f86883ec7880049c94c539e8b20f7693a8273
Njako21 <njako21@student.sdu.dk>
2023-05-09 02:16:11 +0200

    refactoring of endpoints and added endpoints
    change of endpoint route and added endpoints
    Route: /api/f1/year/:year/races
    Example: /api/f1/year/2022/races
    
    Route: /api/f1/year/:year/grand_prix/:grand_prix
    Example: /api/f1/year/2022/grand_prix/Bahrain_Grand_Prix
    
    Route: /api/f1/year/:year/driver/:driver
    Example: /api/f1/year/2022/driver/lewis_hamilton
    
    Route: /api/f1/year/:year/drivers
    Example: /api/f1/year/2022/drivers
    
    Route: /api/f1/driver/:driver
    Example: /api/f1/driver/Lewis_Hamilton

--- node/logic/f1Logic.js
+++ node/logic/f1Logic.js
@@ -1,10 +1,20 @@
 const db = require('../services/db');
 const format = require('../services/formatting');
+const fs = require('fs');
+const path = require('path');
 
+/*  note to self: the callback function is the function that is passed into the function as a parameter
+    the callback function is then called in the function
+    the callback function is used to return the result of the function
+    the callback function is used to return an error if the function fails
 
-// Queries the database for races in a given year and returns the formatted result
-// route /api/f1/:year
-// example /api/f1/2022
+    note not routes but functions that are called by the routes
+*/
+
+/*  Queries the database for races in a given year and returns the formatted result
+    route /api/f1/year/:year
+    example /api/f1/year/2022
+*/
 function yearRaces(year, callback) {
   if (!year) {
     return callback('Year parameter missing');
@@ -21,15 +31,16 @@ function yearRaces(year, callback) {
   });
 }
 
-// Queries the database for the results of a given grand prix in a given year and returns the formatted result
-// route /api/f1/:year/:grand_prix
-// example /api/f1/2022/Bahrain_Grand_Prix
+/*   Queries the database for the results of a given grand prix in a given year and returns the formatted result
+     route /api/f1/year/:year/grand_prix/:grand_prix
+     example /api/f1/year/2022/grand_prix/Bahrain_Grand_Prix
+*/
 function grandPrix(year, grand_prix, callback){
     if (!year || !grand_prix) {
         return callback('Year parameter missing');
     }
     grand_prix = grand_prix.replace(/_/g, ' ');
-    
+
     const query = `
         SELECT drivers.code, drivers.forename, drivers.surname, results.positionText, results.positionOrder, results.grid, status.status 
         FROM races 
@@ -49,7 +60,105 @@ function grandPrix(year, grand_prix, callback){
       });
 }
 
+
+/*   Queries the database for the results of a given driver in a given year and returns the formatted result
+     route /api/f1/year/:year/driver/:driver
+     example /api/f1/year/2022/driver/Max_Verstappen
+*/
+
+function yearDriver(year, driver, callback){
+    if (!year || !driver) {
+        return callback('Year or driver parameter missing');
+    }
+    firstName = driver.split("_")[0];
+    lastName = driver.split("_")[1];
+
+    const query = `
+    SELECT r.round, r.name, r.date, res.positionText, res.positionOrder, res.grid, status.status
+    FROM races r
+    INNER JOIN results res ON r.raceId = res.raceId
+    INNER JOIN drivers d ON res.driverId = d.driverId
+    INNER JOIN status ON res.statusId = status.statusId
+    WHERE d.forename = "${firstName}" AND d.surname = "${lastName}" AND YEAR(r.date) = ${year}
+    ORDER BY r.round ASC;
+    `;
+
+    let finishes = [];
+    db.query(query, (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            return callback('Error executing query');
+        } else {
+            for (let i = 0; i < results.length; i++) {
+                finishes.push(results[i].positionOrder);
+            }
+            results.push(finishes);
+            return callback(null, format(year, results, "driver year"));
+        }
+    });
+}
+
+/*   Queries the database for the results of all drivers in a given year and returns the formatted result
+     route /api/f1/year/:year/drivers
+     example /api/f1/year/2022/drivers
+*/
+
+function yearDrivers(year, callback){
+    if (!year) {
+        return callback('Year or driver parameter missing');
+    }
+
+    const query = `
+    SELECT DISTINCT d.forename, d.surname, d.code, d.driverRef
+    FROM drivers d
+    JOIN results r ON d.driverId = r.driverId
+    JOIN races ra ON ra.raceId = r.raceId
+    WHERE YEAR(ra.date) = ${year}
+    `;
+    db.query(query, (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            return callback('Error executing query');
+        } else {
+            return callback(null, format(year, results, "drivers year"));
+        }
+    });
+}
+
+/* Queries the database for the results of a given driver and returns the formatted result
+   route /api/f1/driver/:driver
+   example /api/f1/driver/Max_Verstappen
+*/
+
+function driver(driver, callback){
+    if (!driver) {
+        return callback('driver parameter missing');
+    }
+    firstName = driver.split("_")[0];
+    lastName = driver.split("_")[1];
+    const query = `
+    SELECT r.positionOrder, r.positionText, r.grid, s.status, ra.round, ra.name, ra.year 
+    FROM results r
+    JOIN drivers d ON d.driverId = r.driverId
+    JOIN races ra ON ra.raceId = r.raceId
+    JOIN status s ON s.statusId = r.statusId
+    WHERE d.forename = "${firstName}" AND d.surname = "${lastName}"
+    `;
+    db.query(query, (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            return callback('Error executing query');
+        } else {
+            return callback(null, format(0000, results, `Driver ${driver}`));
+        }
+    });
+}
+
+
 module.exports = {
   yearRaces: yearRaces,
-  grandPrix: grandPrix
+  grandPrix: grandPrix,
+  yearDriver: yearDriver,
+  yearDrivers: yearDrivers,
+  driver: driver,
 };
--- node/routes/f1.js
+++ node/routes/f1.js
@@ -3,103 +3,8 @@ const router = express.Router();
 const f1Logic = require('../logic/f1Logic');
 const db = require('../services/db');
 
+
 //Define routes for /api/f1
-router.get('/year/:year', (req, res) => {
-  const year = req.params.year;
-
-  if (!year || year.length !== 4) {
-    res.status(400).send('not valid year parameter. "' + year + '"');
-    return;
-  }
-
-  const query = ''+
-  'SELECT r.raceId, ds.driverId, ds.position, d.forename, d.surname '+
-  'FROM races r ' +
-  'JOIN driverstandings ds ON r.raceId = ds.raceId ' +
-  'JOIN drivers d ON ds.driverId = d.driverId ' + 
-  "WHERE r.Year = " + Number(year) + "";
-
-  console.log(query);
-
-  db.query(query, (error, results) => {
-    if (error) {
-      console.error('Error executing query:', error);
-      res.status(500).send('Error executing query');
-    } else {
-      console.log("query done and results gotten");
-      const drivers = results.map((result) => {
-        return {
-          position: result.position,
-          name: result.forename + ' ' + result.surname,
-        };
-      });
-      console.log("loop one");
-      for (let i = 0; i < drivers.length; i++) {
-        for (let j = i + 1; j < drivers.length; j++) {
-          if (drivers[i].name === drivers[j].name) {
-            drivers[i].position = drivers[i].position + ', ' + drivers[j].position;
-            drivers.splice(j, 1);
-            j--;
-          }
-        }
-      }
-      
-      pointSystem = {
-        1: 25,
-        2: 18,
-        3: 15,
-        4: 12,
-        5: 10,
-        6: 8,
-        7: 6,
-        8: 4,
-        9: 2,
-        10: 1,
-      };
-      console.log("loop two");
-      for (let i = 0; i < drivers.length; i++) {
-        const positions = drivers[i].position.split(', ');
-        let points = 0;
-        for (let j = 0; j < positions.length; j++) {
-          if (positions[j] <= 10) {
-            points += pointSystem[positions[j]];
-          }
-        }
-        drivers[i].points = points;
-      }
-      console.log("sending");
-      res.send(drivers);
-    }
-
-  });
-});
-
-
-
-// driver points
-router.get('/:driver', (req, res) => {
-  const driver = req.params.driver;
-  
-  if (!driver) {
-    res.status(400).send('Driver parameter missing');
-    return;
-  }
-
-  const query = ''+
-  'SELECT points '+
-  'FROM drivers '+
-  'JOIN driverstandings ON drivers.driverId = driverstandings.driverId '+
-  "WHERE drivers.driverRef = '" + driver + "' ";
-  db.query(query, (error, results) => {
-    if (error) {
-      console.error('Error executing query:', error);
-      res.status(500).send('Error executing query');
-    } else {
-      res.send(results);
-    }
-
-  });
-});
 
 //the routes we are working on for the project
 //         |
@@ -107,9 +12,9 @@ router.get('/:driver', (req, res) => {
 //        \ /
 //         V
 
-// route /api/f1/:year
-// example /api/f1/2022
-router.get('/:year/races', (req, res) => {
+// route    /api/f1/year/:year
+// example  /api/year/f1/2022
+router.get('/year/:year/races', (req, res) => {
   const year = req.params.year;
   f1Logic.yearRaces(year, (error, result) => {
     if (error) {
@@ -120,9 +25,9 @@ router.get('/:year/races', (req, res) => {
   });
 });
 
-// route /api/f1/:year/:grand_prix
-// example /api/f1/2022/Bahrain_Grand_Prix
-router.get('/:year/:grand_prix', (req, res) => {
+// route    /api/f1/year/:year/grand_prix/:grand_prix
+// example  /api/f1/year/2022/grand_prix/Bahrain_Grand_Prix
+router.get('/year/:year/grand_prix/:grand_prix', (req, res) => {
   const year = req.params.year;
   const grand_prix = req.params.grand_prix;
 
@@ -134,5 +39,51 @@ router.get('/:year/:grand_prix', (req, res) => {
     }
   });
 });
+
+
+// route    /api/f1/year/:year/driver/:driver
+// example  /api/f1/year/2022/driver/lewis_hamilton
+router.get('/year/:year/driver/:driver', (req, res) => {
+  const year = req.params.year;
+  const driver = req.params.driver;
+
+  f1Logic.yearDriver(year, driver, (error, result) => {
+    if (error) {
+      res.status(400).send(error);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+
+// route    /api/f1/year/:year/drivers
+// example  /api/f1/year/2022/drivers
+router.get('/year/:year/drivers', (req, res) => {
+  const year = req.params.year;
+
+  f1Logic.yearDrivers(year, (error, result) => {
+    if (error) {
+      res.status(400).send(error);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+// route    /api/f1/driver/:driver
+// example  /api/f1/driver/Lewis_Hamilton
+router.get('/driver/:driver', (req, res) => {
+  const driver = req.params.driver;
+
+  f1Logic.driver(driver, (error, result) => {
+    if (error) {
+      res.status(400).send(error);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
 
 module.exports = router;
