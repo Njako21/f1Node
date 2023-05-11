a5ab0f4008e2621cd8a115b9f47bdd4b5420e7e6
JulieSonnichsen <93269568+JulieSonnichsen@users.noreply.github.com>
2023-05-11 11:56:00 +0200

    Frontend yeet

--- node/index.js
+++ node/index.js
@@ -7,6 +7,13 @@ const db = require('./services/db'); // Import database connection module
 
 // Create an instance of the express application
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const port = process.env.PORT || 3000; // Set the port number for the server
 
 // Add middleware to parse incoming requests with JSON payloads
